f9b9366360197c31c6e72df20f8606a11bce0cc4
ronghui <ronghui@cpirhzl.com>
2023-02-16 09:32:41 +0800

    添加eureka注册中心（本系统准备集成nacos做注册与配置中心， 前期开发阶段，暂时用eureka）

---
+++ eureka/src/main/java/com/cpirh/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.cpirh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author ronghui
+ * @Description
+ * @date 2023/2/13 15:32
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
