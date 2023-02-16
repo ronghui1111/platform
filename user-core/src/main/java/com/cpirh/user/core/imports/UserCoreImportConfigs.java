7aca76a244608c79a6bdf1eff8ffbf164f95131c
ronghui <ronghui@cpirhzl.com>
2023-02-16 09:31:26 +0800

    添加用户信息核心依赖包，业务模块需要引用此依赖包，保证服务中顺利的获取用户信息，且加入feign拦截器，保证服务与服务直接的调用传递用户信息

---
+++ user-core/src/main/java/com/cpirh/user/core/imports/UserCoreImportConfigs.java
@@ -0,0 +1,12 @@
+package com.cpirh.user.core.imports;
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author ronghui
+ * @Description
+ * @date 2023/2/15 16:21
+ */
+@ComponentScan("com.cpirh.user.core")
+public class UserCoreImportConfigs {
+}
